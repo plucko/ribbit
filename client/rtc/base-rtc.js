1ead6534beba3b527b3a6f0758355eb86be0d4f0
Andy Kitson <andrew.kitson@gmail.com>
2015-03-19 00:51:23 -0700

    Clean up comments

--- client/rtc/base-rtc.js
+++ client/rtc/base-rtc.js
@@ -9,27 +9,34 @@
 // initializing the base class or extend createConnectionTo.
 
 var makeBaseRTC = function (options) {
-
   var baseRTC = {};
+
+  // Connect to our signal server. We talk with other clients over 
+  // this server in order to establish peer connections to them.
   baseRTC.signalServer = new WebSocket(options.signalServer);
+
+  // Save off config RTCPeerConnection needs to know how to connect
+  // to other clients
   baseRTC.peerConnectionConfig = options.peerConnectionConfig;
 
-  // Index peer connections by remote user.
+  // Maintain multiple peer connections, indexed by remote user
   baseRTC.peerConnections = {};
   
-  // Get local media stream, store it in this.localStream, and add it to all peer connections.
+  // If this client will be streaming media to a peer, getMedia should be
+  // called before initiating the handshake (offer/answer) process. (Adding
+  // a stream after connection is established requires the connection to be 
+  // renegotiated, a process we don't support now.)
+  //
+  // This method retrieves the media streams specified in the constraints
+  // parameter. It saves the streams so we can pass them to our peer connections 
+  // as we connect to them later.
   baseRTC.getMedia = function (constraints, success, error) {
     constraints = constraints || { video: true, audio: true };
     error = error || function () {};
     
     var mediaSuccess = function (stream) {
       this.localStream = stream; 
-      // Add our media stream to any peer connections we already have
-      for (var remote in this.peerConnections) {
-        this.peerConnections[remote].addStream(stream);
-      }
-      // Allow caller to do what they want with the stream
-      success && success(stream);  //onmedia callback -- turn into an event
+      success && success(stream);
     }.bind(this);
 
     navigator.getUserMedia(constraints, mediaSuccess, error)
@@ -72,19 +79,25 @@ var makeBaseRTC = function (options) {
     return pc;
   };
 
-  // Subclasses must implement -- some will connect to a room, others to a user!
+  // Subclasses must implement -- some will connect to a room, others to a user.
+  // In either case, the room is the room they are connecting to. It takes an 
+  // object of the form: { name: 'living room', presenter: 'fred' }. The user
+  // is the user they are connecting as (not who they are connecting to).
   baseRTC.connect = function (room, user) {
-  }
-
-  // Issue a request to all clients in room to send an offer. Clients respond
-  // with an offer that we can then give an answer to. As a rule we only create
-  // offers for individual users, so if we don't know who the users are, we just 
-  // as a group to send us offers.
-  baseRTC.connectToRoom = function (room) {
-    this._send({ 'requester': this.me }, 'everyone', 'request-for-offer'); //TODO: don't use 'everyone' as user. Find a better way to indicate we want to send a message to all clients
+    console.log('ERROR: called connect method on base class. Subclasses must implement .connect');
   };
 
-  // Create peer connection to a specific user
+  // Ask all clients in room to send an offer. They all generate offers that
+  // we can then give an answer to. Why do this? Because we don't know who else
+  // is in the room, so we don't know what peer connections to generate so we can 
+  // create offers. So instead, we ask for offers and now we know which peer 
+  // connections to create.
+  // TODO: fix the hacky way we're sending a message to the entire room. It shouldn't go to 'everyone'
+  baseRTC.connectToRoom = function (room) {
+    this._send({ 'requester': this.me }, 'everyone', 'request-for-offer'); 
+  };
+
+  // Create peer connection to a specific user and send them an offer. 
   baseRTC.connectToUser = function (remoteUser) {
     var pc = this.getPeerConnection(remoteUser);
     pc.createOffer(function(description) {
@@ -94,7 +107,10 @@ var makeBaseRTC = function (options) {
     }.bind(this), function (err) { console.log('offer erorr: ', err)});
   };
 
-  // Helper method to send a message to a specific recipient
+  // Helper method to send a message to a specific recipient. Make sure all 
+  // messages are sent with a similar format. In particular, we need all messages
+  // to have a sender, recipient, toom, and type. Otherwise, our onmessage handler
+  // doesn't know what to do. See the handler for details on how these fields are used.
   baseRTC._send = function (message, recipient, messageType) {
     this.signalServer.send(JSON.stringify({
       sender: this.me,
@@ -105,72 +121,87 @@ var makeBaseRTC = function (options) {
     }));
   };
 
-  // See _send for message.data structure
-  baseRTC._gotSignalMessage = function (message) {
+  // Handle incoming messages. Message structure is described in _send.
+  var onMessage = function (message) {
     var data = JSON.parse(message.data);
+
+    // Screen out messages we don't care about. This is a hack to make up 
+    // for the signal server sending everything to everyone. Really, the
+    // signal server should send a message if and only if we need to see it.
+    if (!data.sender) return;
+    if (data.recipient !== this.me && 
+        (data.recipient !== 'everyone' || 
+          data.sender === this.me)) {
+          return;
+    }           
+    // Super useful for debugging. Most issues are somewhere in this function.
     console.log(data);
-    if (!data.sender) return; // just ignore, though ultimately, server shouldn't send these
-    //TODO: find a better way to indicate a message goes to everyone (at least include room check)
-    if (data.recipient !== this.me && (data.recipient !== 'everyone' || data.sender === this.me)) { // || data.room !== this.room)) {
-      return; // not addressed to me... signal server shouldn't have sent to me in the first place
-    }      
-    
+
     // get peer connection for sender (or create one if we don't have one yet)
     var pc = this.getPeerConnection(data.sender);
 
-    // Respond to offer by setting new pc's remote description and sending an answer
-    // that contains our description
+    // Respond to offer with our answer, updating our remote and local descriptions
+    // in the process
     if (data.type === 'offer') {
-      var self = this;
-      var logErr = console.log.bind(console);
+      var self = this; //for _send below
       pc.setRemoteDescription(new RTCSessionDescription(data.contents), function () {
         pc.createAnswer(function (answer) {
           pc.setLocalDescription(answer, function () {
             self._send(answer, data.sender, 'answer');
-          }, logErr);
-        }, logErr);
-      }, logErr);
+          });
+        });
+      });
 
     // Respond to answers by just updating our remote description 
     } else if (data.type === 'answer') {
       pc.setRemoteDescription(new RTCSessionDescription(data.contents));
-    // Add ICE Candidates as they come in
+
+    // Add ICE Candidates as they come in -- the remote peer's oneicecandidate handler sends these
     } else if (data.type === 'ice') {
       pc.addIceCandidate(new RTCIceCandidate(data.contents.ice));
+
     // Send an offer if asked  
     } else if (data.type === 'request-for-offer') {
       this.connectToUser(data.contents.requester);
+
+    // Oops! Where'd this message come from?
+    } else {
+      console.log('baseRTC:onMessage - unhandled message type: ' + data.type, data);
     }
   };
+  
+  // Use onMessage to handle messages coming from signal server
+  baseRTC.signalServer.onmessage = onMessage.bind(baseRTC);
 
-  baseRTC.signalServer.onmessage = baseRTC._gotSignalMessage.bind(baseRTC);
-
+  // Peer connections emit events, like onaddstream and onremovestream.
+  // We need to provide a way to allow consumers of our service to 
+  // register handlers for these events. This is how we're doing it 
+  // now -- not super robust, but it works.
   var handlers = baseRTC.handlers = {};
   baseRTC.on = function (event, handler) {
-    //add to existing connections
+    // add to existing connections
     for (var u in this.peerConnections) {
       this.peerConnections[u][event] = handler;
     }
+    // save so we can add to peer connections we create in the future
     handlers[event] = handler //only one handler per event for now
-    //save so we can add to future connections
-    // handlers[event] = handlers[event] || [];
-    // handlers[event].push(handler);
   };
   return baseRTC;
 
 };
 
-
-
+// ********************
+//    Angularization
+// ********************
 // We use a provider because there is app-level configuration
 // that should not be hardcoded. Specifically, you must set:
-// - The url of your signal server (requires a socket connection)
+// - The url of your signal server (which must use sockets)
 // - The ICE servers that peer connections will use in figuring out
 //   how to talk to each other.
 //
 // This is what a valid config looks like, though your specific config
 // will be different:
-//
+// ```
 // angular.module('yourApp')
 //  .config(['baseRTCProvider', function(baseRTCProvider) {
 //     baseRTCProvider.setSignalServer('ws://localhost:3434'); 
@@ -181,6 +212,7 @@ var makeBaseRTC = function (options) {
 //       ]
 //     });
 //   }]);
+// ```
 
 angular.module('ribbitBaseRTC', [])
   .provider('baseRTC', function () {
