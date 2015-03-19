b28bce38dc3fe38dee3d711bc7738e80230f304e
Andy Kitson <andrew.kitson@gmail.com>
2015-03-18 23:06:34 -0700

    Move test controllers from service code to test html files

--- client/rtc/audience-rtc.js
+++ client/rtc/audience-rtc.js
@@ -6,27 +6,13 @@
 
 angular.module('ribbitAudienceRTC', ['ribbitBaseRTC'])
   .factory('audienceRTC', function (baseRTC) {
-
     // Get media right away for audience members
     baseRTC.connect = function (room, user) {
       this.room = room;
       this.me = user;
       this.connectToUser(room.presenter);
     }
-    // baseRTC.getMedia({ video: true, audio: true}, function (stream) {
-    //   document.getElementbyId('local-video').src = window.URL.createObjectURL(stream)
-    // });
     return baseRTC;
-  })
-  .controller('audienceCtrl', function (audienceRTC) {
-    audienceRTC.signalServer.onopen = function () {  //shouldn't have to mess with this here
-      console.log('audience trying to connect')
-      audienceRTC.getMedia({ video: true, audio: true }, function () {
-        //let's see if this works -- only connecting after getMedia
-        audienceRTC.connect({ roomname: 'aviary', presenter: 'fred' }, 'andy3');
-      });
-    }
-  })
-
+  });
 
 
